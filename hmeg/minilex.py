7b2e31930df89c69ad2884261473ece758df5a32
yury <yurytsoy@gmail.com>
2024-03-01 21:31:31 +0900

    Start filling-in minilex

---
+++ hmeg/minilex.py
@@ -0,0 +1,34 @@
+class WordClasses:
+    """
+    From Gunnemark's minilex
+    """
+    Colors = "colors"
+    DaysOfWeek = "days of week"
+    PointsOfTheCompass = "points of the compass"  # east, east of, ...
+    Numbers = "numbers"  # I, you, ...
+    OrdinalNumbers = "ordinal numbers"  # first, second, ...
+    PersonalPronoun = "personal pronoun"  # I, you, ...
+    PossessivePronoun = "possessive pronoun"  # my, your, ...
+    Seasons = "seasons"  # spring, ...
+
+
+verbs = [
+    "arrive", "ask",
+    "become", "begin", "believe", "borrow", "bring", "buy",
+    "clean", "come", "continue", "cry",
+    "damage", "do", "drop",
+    "eat",
+    "fall", "feel", "find", "finish", "forget", "free",
+    "have", "head", "hold", "hope", "hurt",
+    "joke",
+    "keep", "know",
+    "laugh", "learn", "leave", "lend", "let", "lie", "like", "listen", "live", "look", "look for", "love",
+    "make", "mean", "meet",
+    "name", "need",
+    "pay", "place", "play", "put",
+    "read", "remember",
+    "say", "see", "sell", "send", "show", "shut", "sing", "speak", "stand", "stay", "stop", "suggest",
+    "take", "talk", "teach", "think", "travel",
+    "understand", "use",
+    "wait", "walk", "want", "work", "worry", "write"
+]
