6b45d26d6be496edb8704c4c50eab80f97147000
yury <yurytsoy@gmail.com>
2024-03-09 20:16:13 +0900

    Refactor grammar placeholders for the 3rd face singular

--- hmeg/entities.py
+++ hmeg/entities.py
@@ -1,12 +1,23 @@
+from __future__ import annotations
 import dataclasses
 
 
 class MinilexPlaceholders:
     Noun = "{noun}"  # noun, singular
     Pronoun = "{pronoun}"  # personal pronoun: I, we, they, you
-    PronounSingular3rd = "{pronoun:s}"  # personal pronoun for 3rd person, singular: he, she, it
+    PronounSingular3rd = "{pronoun:3s}"  # personal pronoun for 3rd person, singular: he, she, it
     Verb = "{verb}"  # verb, present simple
-    VerbSingular3rd = "{verb:s}"  # verb in the present simple for 3rd person, singular
+    VerbSingular3rd = "{verb:3s}"  # verb in the present simple for 3rd person, singular
+
+    @staticmethod
+    def to_list() -> list[str]:
+        return [
+            MinilexPlaceholders.Noun,
+            MinilexPlaceholders.Pronoun,
+            MinilexPlaceholders.PronounSingular3rd,
+            MinilexPlaceholders.Verb,
+            MinilexPlaceholders.VerbSingular3rd
+        ]
 
 
 @dataclasses.dataclass
