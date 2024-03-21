c7273d0dda119cb7407ab3742493543a51930fde
yury <yurytsoy@gmail.com>
2024-03-21 22:44:25 +0900

    refactor "grammar" to "topics" for more consistent terminology

--- hmeg/utils.py
+++ hmeg/utils.py
@@ -14,7 +14,7 @@ def register_grammar_topics(grammar_dir: str | None = None):
     """
 
     # iterate over files in hmeg.grammar, load descriptions and register in the factory.
-    grammar_dir = grammar_dir or "hmeg/grammar/"
+    grammar_dir = grammar_dir or "hmeg/topics/"
 
     for file in os.listdir(grammar_dir):
         if not file.endswith(".toml"):
