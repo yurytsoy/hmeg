62eae6ef6c2affe25545b49ce5cf6f733b2b1345
yurytsoy <yurytsoy@gmail.com>
2026-01-15 22:36:46 +0900

    Use mocks for OpenAI tests

--- tests/test_reranker.py
+++ tests/test_reranker.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from unittest.mock import patch
 
 from dotenv import load_dotenv
 
@@ -195,63 +196,32 @@ class TestReranker(unittest.TestCase):
     def test_rank_openai(self):
         Reranker.set_current_model(Reranker.Models.openai)
 
-        with self.subTest("Short sentence-1"):
-            sorted_res = Reranker.rank(
+        with patch("hmeg.reranker.Reranker.rank_openai", return_value=[('fox', -1.0)]) as ranker:
+            kwargs = dict(
                 context="Quick brown foks jumped",
                 original="foks",
                 replacements=["box", "fox", "sox", "crocs"]
             )
+            sorted_res = Reranker.rank(**kwargs)
+            self.assertEqual(ranker.call_count, 1)
             expected = ('fox', -1.0)
             self.assertEqual(sorted_res[0], expected)
-
-        with self.subTest("Short sentence-2"):
-            sorted_res = Reranker.rank(
-                context="Quick brown fox jumpd",
-                original="jumpd",
-                replacements=["dumped", "bumped", "jumped", "crocs"]
-            )
-            expected = ('jumped', -1.0)
-            self.assertEqual(sorted_res[0], expected)
-
-        with self.subTest("Long phrase"):
-            sorted_res = Reranker.rank(
-                context="All year long, the grasshopper kept burying acorns for winter while the oktopus mooched off his girlfriend and watched TV.",
-                original="oktopus",
-                replacements=["aktopus", "octopus", "octocat", "crocs"]
-            )
-            expected = ('octopus', -1.0)
-            self.assertEqual(sorted_res[0], expected)
+            kwargs["full_sentence_score"] = False
+            self.assertEqual(ranker.call_args[1], kwargs)
 
     @unittest.skipIf(os.getenv("OPENAI_API_KEY") is None, "OPENAI_API_KEY is not set")
     def test_rank_openai_full_context(self):
         Reranker.set_current_model(Reranker.Models.openai)
 
-        with self.subTest("Short sentence-1"):
-            sorted_res = Reranker.rank(
+        with patch("hmeg.reranker.Reranker.rank_openai", return_value=[('fox', -1.0)]) as ranker:
+            kwargs = dict(
                 context="Quick brown foks jumped",
                 original="foks",
                 replacements=["box", "fox", "sox", "crocs"],
                 full_sentence_score=True
             )
+            sorted_res = Reranker.rank(**kwargs)
+            self.assertEqual(ranker.call_count, 1)
             expected = ('fox', -1.0)
             self.assertEqual(sorted_res[0], expected)
-
-        with self.subTest("Short sentence-2"):
-            sorted_res = Reranker.rank(
-                context="Quick brown fox jumpd",
-                original="jumpd",
-                replacements=["dumped", "bumped", "jumped", "crocs"],
-                full_sentence_score=True
-            )
-            expected = ('jumped', -1.0)
-            self.assertEqual(sorted_res[0], expected)
-
-        with self.subTest("Long phrase"):
-            sorted_res = Reranker.rank(
-                context="All year long, the grasshopper kept burying acorns for winter while the oktopus mooched off his girlfriend and watched TV.",
-                original="oktopus",
-                replacements=["aktopus", "octopus", "octocat", "crocs"],
-                full_sentence_score=True
-            )
-            expected = ('octopus', -1.0)
-            self.assertEqual(sorted_res[0], expected)
+            self.assertEqual(ranker.call_args[1], kwargs)
