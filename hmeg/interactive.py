c006671fa16809ff1bad8b61a7ec6645b875d16d
yurytsoy <yurytsoy@gmail.com>
2026-02-24 22:19:08 +0900

    support tutor in CLI

--- hmeg/interactive.py
+++ hmeg/interactive.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from hmeg.tutor.main_loop import chat_loop
-from hmeg.tutor.usecases import create_agent, extract_messages_from_checkpointer
+from hmeg.tutor.usecases import create_agent
 
 
 if __name__ == "__main__":
